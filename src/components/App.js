2de339cd5754b0119833fd8f22082f3276885f87
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-20 05:56:51 +0300

    completed Assigment

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,18 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home
+        city={user.city}
+        color={user.color}
+        bio={user.bio}
+        name={user.name}
+      />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
